616833153d2c569e249b7668f157dd0ac5d2674c
DITNO <92884446+DITNO@users.noreply.github.com>
2023-03-31 17:28:03 +0530

    to convert text into upper case or lower case

---
+++ Uppercase_&lowercase.js
@@ -0,0 +1,7 @@
+
+var essay = prompt('enter ur tweet');
+var a = essay.length;
+var b = 140- a;
+alert('you have written'+' '+a+''+'and left with '+ b);
+alert(essay.slice(0,140));
+alert(essay.toUpperCase());
