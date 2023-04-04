23dcbc1adefd3395e543c2eb37278ff0ca5d3e2c
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-04 15:16:35 +0530

    usage of random function

---
+++ love_calc.js
@@ -0,0 +1,7 @@
+alert("!!!!! WELCOME TO LOVE CALCULATOR !!!!!")
+prompt('Enter name 1');
+prompt('Enter name 2');
+var n = Math.random();
+n = n*100+1;
+n = Math.floor(n);
+alert('Your love percentage is '+n+"%");
