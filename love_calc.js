3bbb6aebdd1edaef4770f6f2740e0b96b730df9e
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-04 15:23:54 +0530

    adding if else

--- love_calc.js
+++ love_calc.js
@@ -5,3 +5,12 @@ var n = Math.random();
 n = n*100+1;
 n = Math.floor(n);
 alert('Your love percentage is '+n+"%");
+if(n === 100){
+    alert('U R LUCKY');
+}
+else if (n <100 && n>70){
+    alert('good going');
+}
+else {
+    alert('ALL THE BEST BRUHHHH');
+}
