9246a3479e242ed94703efe1809cc713bf77a6e9
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-07 01:02:00 +0530

    random selection of person paying

---
+++ random_personpay.js
@@ -0,0 +1,17 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+var num = names.length;
+var random = Math.floor(Math.random()*num);
+var people = names[random];
+return people +' is going to buy lunch today!';
+    
+    
+    
+ 
+
+
+/******Don't change the code below*******/    
+}
